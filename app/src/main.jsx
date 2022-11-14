517dde7273ed768a2e5541018d77c7a6d6efe267
Iker-Romero <ikersoftdev@gmail.com>
2022-11-14 18:24:56 +0100

    Frontend structure

---
+++ app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./routes/Root";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
