4453db75f6d297b40acfb5120b1c6c8de602d57b
Iker-Romero <ikersoftdev@gmail.com>
2022-11-14 18:42:16 +0100

    Color Palette

---
+++ app/src/styles/theme.jsx
@@ -0,0 +1,9 @@
+const theme = {
+  const COLOR_PALETTE = {
+    primary: '#252F3D',
+    secondary: '#146eb4',
+    background: '#252F3D',
+    background2: '#252F3D',
+    highlight: '#ff9900'
+  }
+}
