a07037f75582e54669a336486d09b2186540d75a
Iker-Romero <ikersoftdev@gmail.com>
2022-11-14 18:35:11 +0100

    GlobalStyle from Styled-Components

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Root from "./routes/Root";
+import GlobalStyle from "styled-components";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyle />
     <Root />
   </React.StrictMode>
 );
