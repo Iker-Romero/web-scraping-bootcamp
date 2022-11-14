83ed331dafd4434ba3bb9586259b98d674e903a1
Iker-Romero <ikersoftdev@gmail.com>
2022-11-14 17:05:01 +0100

    Scrapping ready

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 // twotabsearchtextbox
-const URL = "https://www.amazon.com/-/es";
+const URL = "https://www.amazon.es/";
 
 (async () => {
   // Abrimos el navegador
@@ -10,7 +10,7 @@ const URL = "https://www.amazon.com/-/es";
   const page = await browser.newPage();
   // Accedemos a la URL
   await page.goto(URL);
-//   await page.click("#sp-cc-accept");
+  //   await page.click("#sp-cc-accept");
   await page.screenshot({ path: "./screenshots/amazon-home.jpg" });
 
   await page.type("#twotabsearchtextbox", "iphone");
@@ -20,10 +20,14 @@ const URL = "https://www.amazon.com/-/es";
     (async () => {
       page.screenshot({ path: "./screenshots/search-result.jpg" });
 
-      const products = await page.$$eval(".a-section", (nodes) => {
-        console.log(nodes);
+      const products = await page.$$eval(".s-card-container", (nodes) => {
         return nodes.map((node) => ({
-          title: node.querySelector(".s-title-instructions-style > h2 > a > span")?.innerText,
+          title: node.querySelector(
+            "span.a-size-base-plus.a-color-base.a-text-normal"
+          )?.innerText,
+          img: node.querySelector(".s-image")?.src,
+          price: node.querySelector(".a-price-whole")?.innerHTML,
+          review: node.querySelector(".a-icon-alt")?.innerHTML,
         }));
       });
 
