8ef2318bd206eb910a6ecf5e7033f412ce1b3cad
Iker-Romero <ikersoftdev@gmail.com>
2022-11-14 19:04:52 +0100

    Site Router with Home component

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Root from "./routes/Root";
 import GlobalStyle from "styled-components";
+import Home from "./routes/Home";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <Root />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ app/src/routes/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>HOME</h1>;
+};
+
+export default Home;
